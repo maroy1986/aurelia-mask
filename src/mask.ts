86cd6027640a4c3f3b6d8d6c363bad57daebf356
Marc-Andre Roy <maroy@omniscient.ca>
2017-02-21 14:36:44 -0500

    Add editorconfig and apply formatting.

--- src/mask.ts
+++ src/mask.ts
@@ -1,16 +1,18 @@
-﻿import {getMasker} from "./masker";
+﻿import { valueConverter } from "aurelia-framework";
+import { getMasker } from "./masker";
 
+@valueConverter("mask")
 export class MaskValueConverter {
-    toView(value: string, format: string, bindMasking: boolean, placeholder: string): string {
-        var masker = getMasker({maskFormat: format, bindMasking: bindMasking, placeholder: placeholder, aspnetMasking: false});
-        var result = masker.maskValue(value);
-        return result;
-    }
-    fromView(value: string, format: string, bindMasking: boolean, placeholder: string): string {
-        var masker = getMasker({maskFormat: format, bindMasking: bindMasking, placeholder: placeholder, aspnetMasking: false});
-        var result = masker.unmaskValue(value);
-        return result;
-    }
+  toView(value: string, format: string, bindMasking: boolean, placeholder: string): string {
+    var masker = getMasker({ maskFormat: format, bindMasking: bindMasking, placeholder: placeholder, aspnetMasking: false });
+    var result = masker.maskValue(value);
+    return result;
+  }
+  fromView(value: string, format: string, bindMasking: boolean, placeholder: string): string {
+    var masker = getMasker({ maskFormat: format, bindMasking: bindMasking, placeholder: placeholder, aspnetMasking: false });
+    var result = masker.unmaskValue(value);
+    return result;
+  }
 }
 
 
